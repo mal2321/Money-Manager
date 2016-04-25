baf8ee3521c4d7711bd1ad878cc461a1732f9e32
hamal <hamalsaha@gmail.com>
2016-04-25 09:41:41 +0700

    Start App

--- app/src/main/java/com/example/hamal/moneymanager/SignUpActivity.java
+++ app/src/main/java/com/example/hamal/moneymanager/SignUpActivity.java
@@ -1,13 +1,21 @@
 package com.example.hamal.moneymanager;
 
+import android.content.Context;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
