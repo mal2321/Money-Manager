2cb34fd404b7b86b0872cb136fa07a889d1021d7
hamal <hamalsaha@gmail.com>
2016-04-30 17:01:59 +0700

    Start App

--- app/src/main/java/com/example/hamal/moneymanager/SignUpActivity.java
+++ app/src/main/java/com/example/hamal/moneymanager/SignUpActivity.java
@@ -1,8 +1,13 @@
 package com.example.hamal.moneymanager;
 
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -14,6 +19,31 @@ public class SignUpActivity extends AppCompatActivity {
         setContentView(R.layout.activity_signup);
 
         toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("Sign Up");
+        toolbar.setTitleTextColor(Color.WHITE);
         setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+    }
+
+    public void onBackPressed() {
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle("Exit");
+        dialog.setCancelable(true);
+        dialog.setMessage("Do you want to exit?");
+        dialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        });
+
+        dialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        dialog.show();
     }
 }
